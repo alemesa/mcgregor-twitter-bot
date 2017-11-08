4b30aeb0996fb53f09b3f64085d65a965cb58d49
Alejandro Mesa <alejandro.mesa1991@gmail.com>
2017-11-08 16:09:52 -0500

    quotes

--- bot.js
+++ bot.js
@@ -8,16 +8,12 @@ var Bot = new TwitterBot({
   access_token_secret: process.env.BOT_ACCESS_TOKEN_SECRET
 });
 var phraseArray = [
-  'hey twitter',
-  'im tweeting',
-  'tweet tweet',
-  'tweetstorm time... 1/22',
-  'plz RT v important',
-  'delete ur account',
-  'it me',
-  'same',
-  '#dogpants go on 4 legs!!',
-  '#thedress is blue and black'
+  'You might win some, you might lose some. But you go in, you challenge yourself, you become a better man, a better individual, a better fighter.',
+  'I enjoy competition. I enjoy challenges. If a challenge is in front of me and it appeals to me, I will go ahead and conquer it.',
+  'Excellence is not a skill. Excellence is an attitude.',
+  'Failure is not an option for me. Success is all I envision.',
+  'My success isn’t a result of arrogance. It’s a result of belief.',
+  'We’re not just here to take part. We’re here to take over.'
 ];
 function chooseRandom(myArray) {
   return myArray[Math.floor(Math.random() * myArray.length)];
